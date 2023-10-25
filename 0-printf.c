21755ed3b210a69a1bf15c44cfad32e4e6f979f0
Adeboga Abigail <abbyboga@gmail.com>
2023-10-25 13:43:54 -0700

    Print strings to standard output

--- 0-printf.c
+++ 0-printf.c
@@ -21,6 +21,11 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 
+	while(*format)
+	{
+		if (*format != %)
+			write(1, format, 1);
+	}
 	va_end(_printf_args);
 	return (count);
 }
