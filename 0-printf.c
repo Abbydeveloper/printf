08689ed987002011a9572cbb0f85352137283ab1
Adeboga Abigail <abbyboga@gmail.com>
2023-10-25 15:00:24 -0700

    fix error

--- 0-printf.c
+++ 0-printf.c
@@ -1,52 +1,27 @@
 #include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
 #include "main.h"
-
-/**
- * _strlen - Return the length of a string
- * @s: string to return length
- *
- * Return: the length of s
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-
-	return (i);
-}
-
 /**
  * _printf - function that produces output according to a format
  * @format: character string
- *
- * Return: the number of character printed (excluding the null byte used
- * to end output of strings
+ * Return: the number of character printed
  */
-
 int _printf(const char *format, ...)
 {
-	int count = 0;
+	int count = 0, va_list _printf_args;
 
-	va_list _printf_args;
 	va_start(_printf_args, format);
-
 	if (format == NULL)
 		return (-1);
-
-	while(*format)
+	while (*format)
 	{
 		if (*format == '%')
 		{
-			format++;
+			char c, *str, int len;
 
+			format++;
 			if (*format == '\0')
 				break;
-
 			else if (*format == '%')
 			{
 				count++;
@@ -54,29 +29,23 @@ int _printf(const char *format, ...)
 			}
 			else if (*format == 'c')
 			{
-				char c = va_arg(_printf_args, int);
-
+				c = va_arg(_printf_args, int);
 				count++;
 				write(1, &c, 1);
 			}
 			else if (*format == 's')
 			{
-				char *str;
-				int len;
-
 				str = va_arg(_printf_args, char *);
-				len = _strlen(str);
-
-				count += len;
-				write(1, str, len);
-			} 
+				str = (str == NULL) ? "(nul)" : str;
+				count += _strlen(str);
+				write(1, str, _strlen(str));
+			}
 		}
 		else
 		{
 			count++;
 			write(1, format, 1);
 		}
-
 		format++;
 	}
 	va_end(_printf_args);
